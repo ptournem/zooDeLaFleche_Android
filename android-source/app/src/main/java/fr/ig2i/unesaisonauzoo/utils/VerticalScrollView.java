8726ec9c49a741f26daead90bf86e05819d69366
Paul Tournemaine <p.tournemaine@a2si-identit.fr>
2015-05-21 16:21:19 +0200

    ADD : sur l'accueil : textview et encart pour les post facebook

---
+++ android-source/app/src/main/java/fr/ig2i/unesaisonauzoo/utils/VerticalScrollView.java
@@ -0,0 +1,54 @@
+package fr.ig2i.unesaisonauzoo.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+public class VerticalScrollView extends ScrollView{
+
+    public VerticalScrollView(Context context) {
+        super(context);
+    }
+
+    public VerticalScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VerticalScrollView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = ev.getAction();
+        switch (action)
+        {
+            case MotionEvent.ACTION_DOWN:
+                super.onTouchEvent(ev);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                return false; // redirect MotionEvents to ourself
+
+            case MotionEvent.ACTION_CANCEL:
+                super.onTouchEvent(ev);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                return false;
+
+            default:
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        super.onTouchEvent(ev);
+        return true;
+    }
+}
